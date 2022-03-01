8ae57df21492aa0b02d439c0007fc3e03276a110
yumeyuniko <daigocafe@msn.com>
2022-03-02 01:33:52 +0900

    logout

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 
 <img src="./readme.gif" width=70%>
+
+### Logout
+
+
+<img src="./logout.gif" width=80%>
--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,7 @@
 
   <div class="view chat" v-else>
     <header>
-      <button class="logout">Logout</button>
+      <button class="logout" @click="Logout">Logout</button>
       <h1>Welcome, {{ state.username }}</h1>
     </header>
 
@@ -75,6 +75,10 @@ export default {
       }
     };
 
+    const Logout = () => {
+      state.username = "";
+    };
+
     const SendMessage = () => {
       const messagesRef = db.database().ref("messages");
 
@@ -114,6 +118,7 @@ export default {
       state,
       inputMessage,
       SendMessage,
+      Logout,
     };
   },
 };
